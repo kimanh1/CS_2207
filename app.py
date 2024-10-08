2b0b3abfe17b5f8474358ed9791532fef586b9cb
kimanh1 <anh.do.set18@eiu.edu.vn>
2024-10-08 09:41:06 +0700

    news all

--- app.py
+++ app.py
@@ -351,5 +351,5 @@ with col3:
            np.asarray([[0.96, surface,	used_surface,	width,	1856.90,	NB_FLOORS,	LENGTH,	selected_day,	NB_ROOMS,	day_of_week]]))
         st.text(result) 
         st.write( [[0.969802452722392, surface,	used_surface,	width,	1856.904762,	NB_FLOORS,	LENGTH,	selected_day,	NB_ROOMS,	day_of_week]])
-        # cb = joblib.load("cb_model.sav")
-        # st.write(cb.predict([[0.969802452722392, surface,	used_surface,	width,	1856.904762,	NB_FLOORS,	LENGTH,	selected_day,	NB_ROOMS,	day_of_week]]))
+        cb = joblib.load("cb_model.sav")
+        st.write(cb.predict([[0.969802452722392, surface,	used_surface,	width,	1856.904762,	NB_FLOORS,	LENGTH,	selected_day,	NB_ROOMS,	day_of_week]]))
